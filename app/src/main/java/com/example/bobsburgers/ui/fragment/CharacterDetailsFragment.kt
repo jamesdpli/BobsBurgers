4e48b23d65dfa2f31fde25e82f0236f5f68f543b
James <jamesdudhiprice@gmail.com>
2023-02-22 20:55:40 +0000

    Update project to use viewBinding

--- app/src/main/java/com/example/bobsburgers/ui/fragment/CharacterDetailsFragment.kt
+++ app/src/main/java/com/example/bobsburgers/ui/fragment/CharacterDetailsFragment.kt
@@ -2,19 +2,21 @@ package com.example.bobsburgers.ui.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.bobsburgers.R
+import com.example.bobsburgers.databinding.FragmentCharacterDetailsBinding
 import com.example.bobsburgers.ui.viewmodel.CharacterDetailsViewModel
 import com.example.bobsburgers.ui.viewmodel.ViewModelFactory
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
 class CharacterDetailsFragment : Fragment() {
+
+    private var _binding: FragmentCharacterDetailsBinding? = null
+    private val binding get() = _binding!!
 
     @Inject lateinit var viewModelFactory: ViewModelFactory
     private val characterDetailsViewModel by lazy {
@@ -26,17 +28,24 @@ class CharacterDetailsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentCharacterDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
 
         characterDetailsViewModel.getCharacterById("9")
         characterDetailsViewModel.characterDetailsLiveData.observe(viewLifecycleOwner) {
-            Log.d("Hello-Dagger", it.toString())
+            networkCharacter -> binding.testTextView.text = networkCharacter.name
         }
 
-        return layoutInflater.inflate(R.layout.fragment_character_details, container, false)
+        return view
     }
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
